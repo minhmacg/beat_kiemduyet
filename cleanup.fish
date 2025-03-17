3a5ecfda25ed1b2da61d05043327b00232ffe1cf
nnminh <nnminh@localhost.localdomain>
2025-03-17 09:11:38 +0700

    small change

--- cleanup.fish
+++ cleanup.fish
@@ -1,8 +1,8 @@
 #!/usr/bin/fish
 
-rm -rf /home/minhmacg/Documents/beat/json_src/*
-rm -rf /home/minhmacg/Documents/beat/output_tsv/*
-rm -rf /home/minhmacg/Documents/beat/utilities/output/*
-rm -rf /home/minhmacg/Documents/beat/medias/*
-rm -rf /home/minhmacg/Documents/beat/raw_json/*
-rm -rf /home/minhmacg/Documents/beat/cleaned_medias/*
+rm -rf json_src/*
+rm -rf output_tsv/*
+rm -rf utilities/output/*
+rm -rf medias/*
+rm -rf raw_json/*
+rm -rf cleaned_medias/*
--- fishscripts/prepare_data.fish
+++ fishscripts/prepare_data.fish
@@ -1,11 +1,9 @@
 # Download media
 
-cd ~/Documents/beat/
-pwd
 cd raw_json/
 pwd
 for file in *json
-	fish ../fishscripts/download_media.fish $file
-	fish ../fishscripts/clean_1.fish $file > ../json_src/$file
+	fish ../fishscripts/download_media_from_json.fish $file
+	fish ../fishscripts/clean_json.fish $file > ../json_src/$file
 	echo $file
 end
